b5d3d270298e1fdbb27512e53f3784acb95fa2c8
daniw <dani.winz@bluewin.ch>
2014-03-07 09:55:06 +0100

    cleaned up repo

--- firmware/MC_Library/Debug/Lib_Sources/_hcs08/mc9s08jm60_c.d
+++
@@ -1,5 +0,0 @@
-
-Lib_Sources/_hcs08/mc9s08jm60_c.obj: \
-	C:/Daten/Daniel/studium/git_repo/sem4/mc/mc/templates/MC_Library/Lib_Headers/../Lib_Sources/_hcs08/mc9s08jm60.c \
-	C:/Daten/Daniel/studium/git_repo/sem4/mc/mc/templates/MC_Library/Lib_Headers/_hcs08/mc9s08jm60.h 
-
--- firmware/mccar-sync/FLASH/Lib_Sources/_hcs08/mc9s08jm60_c.d
+++
@@ -1,6 +0,0 @@
-
-
-Lib_Sources/_hcs08/mc9s08jm60_c.obj: \
-	C:/Daten/Daniel/elektronik/projekte/mccar-sync/firmware/MC_Library/Lib_Sources/_hcs08/mc9s08jm60.c \
-	C:/Daten/Daniel/elektronik/projekte/mccar-sync/firmware/MC_Library/Lib_Headers/_hcs08/mc9s08jm60.h 
-
