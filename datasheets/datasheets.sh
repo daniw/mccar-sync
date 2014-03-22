3a5d7d954bd2cac470c5b73715be1be3cb4b6e4b
daniw <dani.winz@bluewin.ch>
2014-03-22 22:41:31 +0100

    datasheets

---
+++ datasheets/datasheets.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+### MCU ###
+
+# Datasheet
+wget -nc http://cache.freescale.com/files/microcontrollers/doc/data_sheet/MC9S08JM60.pdf
+
+# HCS08 Family reference manual
+wget -nc http://cache.freescale.com/files/microcontrollers/doc/ref_manual/HCS08RMV1.pdf
+
+
+### Joystick ###
+wget -nc http://www.adafruit.com/datasheets/SKQUCAA010-ALPS.pdf
+
+
+### Motor driver ###
+
+# H bridge
+wget -nc -O A3901.pdf http://www.allegromicro.com/~/media/Files/Datasheets/A3901-Datasheet.ashx
+
+
+### Quadrature encoder ###
+
+# Reflective photointerrupter
+wget -nc -O GP2S60-DS.pdf http://www.sharpsme.com/download/GP2S60-DSpdf
+
+# Encoder microcontroller
+wget -nc http://cache.freescale.com/files/microcontrollers/doc/data_sheet/MC9S08SH8.pdf
+
+
+### Buzzer ###
+wget -nc http://www.murata.com/products/catalog/pdf/p37e.pdf
+
+
+### Obstacle detection and IR communication ###
+
+# IR receiver
+wget -nc http://www.vishay.com/docs/82494/tsop752.pdf
+
+
+### Line sensor ###
+
+# Phototransistor
+wget -nc https://www.kingbrightusa.com/images/catalog/spec/APT2012P3BT.pdf
+
+
+### Color sensor ###
+wget -nc -O tcs3414.pdf http://www.ams.com/eng/content/download/250258/975997/142735
+
+
+### Power supply ###
+
+# Lithium battery controller
+wget -nc -O mcp73831.pdf http://ww1.microchip.com/downloads/en/DeviceDoc/21984F.pdf
+# LDO
+wget -nc -O NCP605-D.pdf http://www.onsemi.com/pub_link/Collateral/NCP605-D.PDF
+# Current amplifier
+wget -nc http://diodes.com/datasheets/ZXCT1022.pdf
+
+
+### Accelerometer ###
+wget -nc http://www.freescale.com/files/sensors/doc/data_sheet/MMA8452Q.pdf
+
+
+### Bluetooth module ###
+wget -nc -O EGBT-045MS-046S.pdf http://www.e-gizmo.com/KIT/images/EGBT-04/EGBT-045MS-046S%20Bluetooth%20Module%20Manual%20rev%201r0.pdf
+
+
+### OLED ###
+wget -nc http://www.displayfuture.com/Display/datasheet/controller/SSD1307.pdf
+wget -nc -O RGS08096032WR001.pdf http://www.eleceltek.ch/pdf/1321453888-0a8e75d4a53622c0a004a82a4accb335.pdf
